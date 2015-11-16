42fce1ff9880e2efbaabe7bc577fde855a07facc
Winston Wu <winxton@gmail.com>
2015-11-15 21:01:03 -0500

    hacky tree

--- learn.py
+++ learn.py
@@ -7,6 +7,7 @@ from scipy import sparse
 global WORD_TO_ID_MAP
 global DOC_ID_TO_LABEL_MAP
 global DOC_WORD_PAIR_EXISTS
+global CACHE
 
 class TreeNode:
     def __init__(self, left, right, feature, pointEstimate):
@@ -16,9 +17,10 @@ class TreeNode:
         self.pointEstimate = pointEstimate
 
 class QueueNode:
-    def __init__(self, node, informationGain, docList):
+    def __init__(self, node, informationGain, wordset, docList):
         self.treeNode = node
         self.informationGain = informationGain
+        self.wordset = wordset
         self.docList = docList
 
     def __cmp__(self, other):
@@ -64,22 +66,28 @@ def informationGainAverage(feature, docList):
     except IndexError: # happens if the word is not in either doc
         return (feature, 0, 0)
 
-def getBestFeatureAndValue(wordList, docList):
+def getBestFeatureAndValue(wordset, docList):
     print "Getting best feature.."
-    results = Parallel(n_jobs=4)(delayed(informationGainAverage)(word, docList) for word in wordList)
-    bestWordInfoGainPair = max(results, key=lambda x: x[1])
-    return bestWordInfoGainPair
+
+    if (len(CACHE)>0):
+        bestWordInfoGainEstimateTuple = CACHE.pop(0)
+    else:
+        results = Parallel(n_jobs=4)(delayed(informationGainAverage)(word, docList) for word in wordset)
+        bestWordInfoGainEstimateTuple = max(results, key=lambda x: x[1])
+
+    print bestWordInfoGainEstimateTuple
+    return bestWordInfoGainEstimateTuple
 
 """ Build the Decision Tree"""
-def decisionTreeLearner(docList):
-    feature, infoGain, estimate = getBestFeatureAndValue(docList)
+def decisionTreeLearner(wordset, docList):
+    feature, infoGain, estimate = getBestFeatureAndValue(wordset, docList)
     root = TreeNode(None, None, feature, estimate)
 
     # A priority queue of leaves by information gain
     pqLeaves = []
 
     # Initiate the priority queue with the root node
-    queueNode = QueueNode(root, infoGain, docList)
+    queueNode = QueueNode(root, infoGain, wordset.difference(set([feature])), docList)
     heapq.heappush(pqLeaves, queueNode)
 
     n = 0
@@ -88,14 +96,48 @@ def decisionTreeLearner(docList):
         treeNode = bestQueueNode.treeNode
 
         # Split news by the feature 0 or 1
-        L = [docId for docId in docList if DOC_WORD_PAIR_EXISTS[docId, WORD_TO_ID_MAP[treeNode.feature]] == 1]
-        R = [docId for docId in docList if DOC_WORD_PAIR_EXISTS[docId, WORD_TO_ID_MAP[treeNode.feature]] == 0]
+        # Stop splitting if all documents are in the same group
+        L = [docId for docId in bestQueueNode.docList if DOC_WORD_PAIR_EXISTS[docId, WORD_TO_ID_MAP[treeNode.feature]] == 1]
+        if len(set([DOC_ID_TO_LABEL_MAP[docId] for docId in L])) == 1: # All same group
+            leftTreeNode = TreeNode(None, None, None, DOC_ID_TO_LABEL_MAP[L[0]])
+        else: 
+            feature, infoGain, estimate = getBestFeatureAndValue(bestQueueNode.wordset, L)
+            leftTreeNode = TreeNode(None, None, feature, estimate)
+            leftQueueNode = QueueNode(leftTreeNode, infoGain, bestQueueNode.wordset.difference(set([feature])), L)
+            heapq.heappush(pqLeaves, leftQueueNode)
+        treeNode.left = leftTreeNode
 
+        R = [docId for docId in bestQueueNode.docList if DOC_WORD_PAIR_EXISTS[docId, WORD_TO_ID_MAP[treeNode.feature]] == 0]
+        if len(set([DOC_ID_TO_LABEL_MAP[docId] for docId in R])) == 1: # All same group
+            rightTreeNode = TreeNode(None, None, None, DOC_ID_TO_LABEL_MAP[R[0]])
+        else: 
+            feature, infoGain, estimate = getBestFeatureAndValue(bestQueueNode.wordset, R)
+            rightTreeNode = TreeNode(None, None, feature, estimate)
+            rightQueueNode = QueueNode(rightTreeNode, infoGain, bestQueueNode.wordset.difference(set([feature])), R) 
+            heapq.heappush(pqLeaves, rightQueueNode)
+        treeNode.right = rightTreeNode
+
+        print "%s - %i Split %i %i" % (treeNode.feature, len(bestQueueNode.docList), len(L), len(R))
         n += 1
 
+    return root
 
 if __name__ == "__main__":
 
+    CACHE = [
+        ('atheism', 0.5007250672835606, 2), 
+        ('evidence', 0.4976069389728536, 2),
+        ('moral', 0.4935248144434943, 2),
+        ('islam', 0.4883180190497184, 2),
+        ('bible', 0.4832592298621504, 2),
+        ('solntze', 0.4778028659949304, 2),
+        ('benedikt', 0.4703020274726135, 2),
+        ('tek', 0.4635862253959928, 2),
+        ('mathew', 0.4571242050545491, 2),
+        ('religion', 0.45081764719703954, 2),
+        ('umd', 0.44256338371231796, 2)
+    ]
+    
     # Read data
 
     A = numpy.loadtxt("trainData.txt")
@@ -109,7 +151,7 @@ if __name__ == "__main__":
     for idx, word in enumerate(open('words.txt', 'r')):
         filteredWord = word.strip('\n')
         WORD_TO_ID_MAP[filteredWord] = idx + 1
-        wordset.update(filteredWord)
+        wordset.add(filteredWord)
 
     docList = numpy.unique(rows)
 
@@ -117,4 +159,4 @@ if __name__ == "__main__":
     for idx, label in enumerate(open('trainLabel.txt', 'r')):
         DOC_ID_TO_LABEL_MAP[idx + 1] = int(label)
 
-    #print getBestFeatureAndValue(wordList, docList)
+    root = decisionTreeLearner(wordset, docList)
